54beed32cf0d309f32bcc6731694769bab41476b
Swapnil Andhale <sw4pn@yahoo.com>
2024-06-09 20:27:30 +0530

    updated with private key

--- client/src/Wallet.jsx
+++ client/src/Wallet.jsx
@@ -1,8 +1,20 @@
 import server from "./server";
+import { secp256k1 } from "ethereum-cryptography/secp256k1.js";
+import { toHex } from "ethereum-cryptography/utils";
 
-function Wallet({ address, setAddress, balance, setBalance }) {
+function Wallet({
+  address,
+  setAddress,
+  balance,
+  setBalance,
+  privateKey,
+  setPrivateKey,
+}) {
   async function onChange(evt) {
-    const address = evt.target.value;
+    const cPrivateKey = evt.target.value;
+    setPrivateKey(cPrivateKey);
+
+    const address = toHex(secp256k1.getPublicKey(cPrivateKey));
     setAddress(address);
     if (address) {
       const {
@@ -19,9 +31,16 @@ function Wallet({ address, setAddress, balance, setBalance }) {
       <h1>Your Wallet</h1>
 
       <label>
-        Wallet Address
-        <input placeholder="Type an address, for example: 0x1" value={address} onChange={onChange}></input>
+        {/* Wallet Address */}
+        Private key
+        <input
+          placeholder="Type an address, for example: 0x1"
+          value={privateKey}
+          onChange={onChange}
+        ></input>
       </label>
+
+      <div className="">Address: {address} </div>
 
       <div className="balance">Balance: {balance}</div>
     </div>
--- server/index.js
+++ server/index.js
@@ -1,15 +1,19 @@
-const express = require("express");
+// const express = require("express");
+// const cors = require("cors");
+
+import express from "express";
+import cors from "cors";
+
 const app = express();
-const cors = require("cors");
 const port = 3042;
 
 app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  "0274b7952daca60c2522efb4cd7017fa5f523280388504b6ebad776f74f02ecbbd": 100,
+  "03ca77d448e29438a67a3531ee4e8cdb8a71c7e3e67e937731f3ffe25830fd1e25": 50,
+  "03dd86d59c0a06de0759aeaa2f763c4814bb753a170b3514f891cefd483d06f253": 75,
 };
 
 app.get("/balance/:address", (req, res) => {
